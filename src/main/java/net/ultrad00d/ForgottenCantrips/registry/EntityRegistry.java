e7644e196514b8ff84d3e08131edbfb3931fc221
Igor <ultradoodakk@gmail.com>
2026-06-27 16:45:55 +0300

    feat: spectral donkey

--- src/main/java/net/ultrad00d/ForgottenCantrips/registry/EntityRegistry.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/registry/EntityRegistry.java
@@ -2,26 +2,34 @@ package net.ultrad00d.ForgottenCantrips.registry;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.ultrad00d.ForgottenCantrips.ForgottenCantrips;
 import net.ultrad00d.ForgottenCantrips.entity.SpectralBoat;
+import net.ultrad00d.ForgottenCantrips.entity.SpectralDonkey;
 
-public final class EntityRegistry
-{
+public final class EntityRegistry {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
             DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, ForgottenCantrips.MOD_ID);
 
     public static final RegistryObject<EntityType<SpectralBoat>> SPECTRAL_BOAT =
             ENTITY_TYPES.register("spectral_boat", () ->
-                    EntityType.Builder.<SpectralBoat>of(SpectralBoat::new, MobCategory.MISC)
+                    EntityType.Builder.of(SpectralBoat::new, MobCategory.MISC)
                             .sized(1.375F, 0.5625F)
                             .clientTrackingRange(10)
-                            .build("spectral_boat")
+                            .build("forgotten_cantrips:spectral_boat")
             );
 
-    private EntityRegistry()
-    {
+    public static final RegistryObject<EntityType<SpectralDonkey>> SPECTRAL_DONKEY =
+            ENTITY_TYPES.register("spectral_donkey", () ->
+                    EntityType.Builder.of(SpectralDonkey::new, MobCategory.CREATURE)
+                            .sized(1.3964844F, 1.5F)
+                            .build("forgotten_cantrips:spectral_donkey")
+            );
+
+    public static void register(IEventBus eventBus) {
+        ENTITY_TYPES.register(eventBus);
     }
 }
