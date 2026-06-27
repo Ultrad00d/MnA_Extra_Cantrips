1480524570a912fb131cfac88007027287fe0a39
Igor <ultradoodakk@gmail.com>
2026-06-27 16:46:40 +0300

    feat: spectral donkey cantrip

--- src/main/java/net/ultrad00d/ForgottenCantrips/registry/CantripRegistry.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/registry/CantripRegistry.java
@@ -5,26 +5,35 @@ import com.mna.api.tools.RLoc;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import static net.minecraft.resources.ResourceLocation.fromNamespaceAndPath;
+import static net.minecraft.resources.ResourceLocation.of;
+
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BedPart;
-import net.ultrad00d.ForgottenCantrips.entity.SpectralBedBlockEntity;
+import net.ultrad00d.ForgottenCantrips.blockentity.SpectralBedBlockEntity;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeMod;
 import net.ultrad00d.ForgottenCantrips.entity.SpectralBoat;
+import net.ultrad00d.ForgottenCantrips.entity.SpectralDonkey;
 
 public class CantripRegistry {
     public static void register() {
@@ -54,6 +63,15 @@ public class CantripRegistry {
                 fromNamespaceAndPath("forgotten_cantrips", "textures/gui/cantrips/spectral_boat.png"),
                 1,
                 CantripRegistry::summonBoat,
+                ItemStack.EMPTY,
+                RLoc.create("manaweave_patterns/knot2"), RLoc.create("manaweave_patterns/diamond")
+        );
+        // Spectral Donkey Cantrip
+        registry.registerCantrip(
+                fromNamespaceAndPath("forgotten_cantrips", "spectral_donkey"),
+                fromNamespaceAndPath("forgotten_cantrips", "textures/gui/cantrips/spectral_donkey.png"),
+                1,
+                CantripRegistry::summonDonkey,
                 ItemStack.EMPTY,
                 RLoc.create("manaweave_patterns/knot2"), RLoc.create("manaweave_patterns/diamond")
         );
@@ -168,7 +186,39 @@ public class CantripRegistry {
             if (boat != null)
             {
                 boat.moveTo(target.x, target.y, target.z, player.getYRot(), 0.0F);
+                boat.setOwnerUUID(player.getUUID());
                 serverLevel.addFreshEntity(boat);
+            }
+        }
+    }
+
+    public static void summonDonkey(Player player, ICantrip cantrip, InteractionHand hand) {
+        HitResult hitResult = player.pick(player.getBlockReach(), 0.0F, false);
+
+//        if (hitResult.getType() != BlockHitResult.Type.BLOCK) {
+//            player.sendSystemMessage(Component.translatable("cantrip.forgotten_cantrips.spectral_donkey.toofar"));
+//            return;
+//        }
+
+        Level level = player.level();
+        if (level instanceof ServerLevel serverLevel) {
+            Vec3 target = hitResult.getLocation();
+            SpectralDonkey spectralDonkey = EntityRegistry.SPECTRAL_DONKEY.get().create(serverLevel);
+            if (spectralDonkey != null) {
+                DifficultyInstance difficultyInstance = serverLevel.getCurrentDifficultyAt(new BlockPos((int) target.x, (int) target.y, (int) target.z));
+                spectralDonkey.finalizeSpawn(serverLevel, difficultyInstance, MobSpawnType.TRIGGERED, null, null);
+                spectralDonkey.setOwnerUUID(player.getUUID());
+                spectralDonkey.invulnerableTime = 60;
+                spectralDonkey.setAge(0);
+                spectralDonkey.tameWithName(player);
+                spectralDonkey.equipSaddle(null);
+                spectralDonkey.moveTo(target.x, target.y, target.z);
+                ItemStack offhandStack = player.getItemInHand(InteractionHand.MAIN_HAND.equals(hand) ? InteractionHand.OFF_HAND : InteractionHand.MAIN_HAND);
+                if (offhandStack.is(Items.GOLDEN_CARROT)) {
+                    spectralDonkey.setPersistent(true);
+                    offhandStack.shrink(1);
+                }
+                serverLevel.addFreshEntity(spectralDonkey);
             }
         }
     }
