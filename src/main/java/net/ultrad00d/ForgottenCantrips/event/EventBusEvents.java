a7db296ca8a49a0d055faa76df4da5b6cd883e41
Igor <ultradoodakk@gmail.com>
2026-06-27 16:45:42 +0300

    feat: spectral-linked menu

---
+++ src/main/java/net/ultrad00d/ForgottenCantrips/event/EventBusEvents.java
@@ -0,0 +1,16 @@
+package net.ultrad00d.ForgottenCantrips.event;
+
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.ultrad00d.ForgottenCantrips.ForgottenCantrips;
+import net.ultrad00d.ForgottenCantrips.entity.SpectralDonkey;
+import net.ultrad00d.ForgottenCantrips.registry.EntityRegistry;
+
+@Mod.EventBusSubscriber(modid = ForgottenCantrips.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class EventBusEvents {
+    @SubscribeEvent
+    public static void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(EntityRegistry.SPECTRAL_DONKEY.get(), SpectralDonkey.createAttributes().build());
+    }
+}
