c40fafa0ee3ed19fb0e9973ba4630ddac4984bea
Igor <ultradoodakk@gmail.com>
2026-07-04 18:06:06 +0300

    fix: Items -> Item registry; removed redundant item entries

--- src/main/java/net/ultrad00d/ForgottenCantrips/registry/ItemRegistry.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/registry/ItemRegistry.java
@@ -9,24 +9,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.ultrad00d.ForgottenCantrips.ForgottenCantrips;
 
-public class ItemsRegistry {
+public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ForgottenCantrips.MOD_ID);
 
-    public static final RegistryObject<Item> ARMOR = ITEMS.register("spectral_armor",
-            () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> SPECTRAL_HELMET = ITEMS.register("spectral_helmet",
-            () -> new ArmorItem(ArmorMaterials.SPECTRAL_ARMOR, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new ArmorItem(SpectralArmorMaterial.SPECTRAL_ARMOR, ArmorItem.Type.HELMET, new Item.Properties()));
 
     public static final RegistryObject<Item> SPECTRAL_CHESTPLATE = ITEMS.register("spectral_chestplate",
-            () -> new ArmorItem(ArmorMaterials.SPECTRAL_ARMOR, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
+            () -> new ArmorItem(SpectralArmorMaterial.SPECTRAL_ARMOR, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
 
     public static final RegistryObject<Item> SPECTRAL_LEGGINGS = ITEMS.register("spectral_leggings",
-            () -> new ArmorItem(ArmorMaterials.SPECTRAL_ARMOR, ArmorItem.Type.LEGGINGS, new Item.Properties()));
+            () -> new ArmorItem(SpectralArmorMaterial.SPECTRAL_ARMOR, ArmorItem.Type.LEGGINGS, new Item.Properties()));
 
     public static final RegistryObject<Item> SPECTRAL_BOOTS = ITEMS.register("spectral_boots",
-            () -> new ArmorItem(ArmorMaterials.SPECTRAL_ARMOR, ArmorItem.Type.BOOTS, new Item.Properties()));
+            () -> new ArmorItem(SpectralArmorMaterial.SPECTRAL_ARMOR, ArmorItem.Type.BOOTS, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
