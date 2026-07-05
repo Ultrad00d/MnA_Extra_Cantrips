b79c88e18a66ff38c35f0e95ec8ab9b4665c8298
alexmgh <alexchatgpt.acg@gmail.com>
2026-07-05 21:15:55 +0300

    refactor(illumination): remove MAX_RADIUS config value

--- src/main/java/net/ultrad00d/ForgottenCantrips/config/IlluminationConfig.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/config/IlluminationConfig.java
@@ -7,19 +7,14 @@ public class IlluminationConfig {
     public static final ForgeConfigSpec SPEC;
 
     public static final ForgeConfigSpec.ConfigValue<Integer> RADIUS_EXT;
-    public static final ForgeConfigSpec.ConfigValue<Integer> MAX_RADIUS;
     public static final ForgeConfigSpec.ConfigValue<Integer> TICK_PERIOD;
 
     static {
         BUILDER.push("Illumination");
 
         RADIUS_EXT = BUILDER
-            .comment("Radius extension of the illumination light cylinder (0-2)")
+            .comment("Radius extension of the illumination light cylinder (0-1)")
             .defineInRange("radius_ext", 0, 0, 1);
-
-        MAX_RADIUS = BUILDER
-            .comment("Maximal radius of the illumination light cylinder (1-15)")
-            .defineInRange("max_radius", 15, 1, 15);
 
         TICK_PERIOD = BUILDER
             .comment("How many ticks between light refreshes (1-10, 10 = 0.5 seconds)")
