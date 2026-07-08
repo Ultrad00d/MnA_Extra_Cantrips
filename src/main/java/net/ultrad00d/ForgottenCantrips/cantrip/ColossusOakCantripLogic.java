608bc22f231c5587c9cf3de584ce6bb86a67faa9
Igor <ultradoodakk@gmail.com>
2026-07-08 14:20:55 +0300

    add: Colossus Oak cantrip logic

---
+++ src/main/java/net/ultrad00d/ForgottenCantrips/cantrip/ColossusOakCantripLogic.java
@@ -0,0 +1,12 @@
+package net.ultrad00d.ForgottenCantrips.cantrip;
+
+import com.mna.api.cantrips.ICantrip;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+
+public class ColossusOakCantripLogic extends CantripLogic {
+    @Override
+    public void run(Player player, ICantrip cantrip, InteractionHand hand) {
+
+    }
+}
