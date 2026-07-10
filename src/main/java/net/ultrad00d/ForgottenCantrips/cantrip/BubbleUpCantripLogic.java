ed857ba4f9d7f8f9f7769b51017bf8ebbc79867d
l1n0n <linar.kasimov07@mail.ru>
2026-07-11 00:02:05 +0300

    fix: change effect duration and implement in bubble column behavior

--- src/main/java/net/ultrad00d/ForgottenCantrips/cantrip/BubbleUpCantripLogic.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/cantrip/BubbleUpCantripLogic.java
@@ -2,10 +2,12 @@ package net.ultrad00d.ForgottenCantrips.cantrip;
 
 import com.mna.api.cantrips.ICantrip;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Blocks;
 import net.ultrad00d.ForgottenCantrips.registry.EffectRegistry;
 
 public class BubbleUpCantripLogic extends CantripLogic {
@@ -17,13 +19,37 @@ public class BubbleUpCantripLogic extends CantripLogic {
             return;
         }
 
+        if (isInBubbleColumn(player)) {
+            player.sendSystemMessage(
+                    Component.translatable("cantrip.forgotten_cantrips.bubble_up.bubble_column"));
+            return;
+        }
+
+        if (!hasEnoughWaterDepth(player)) {
+            player.sendSystemMessage(
+                    Component.translatable("cantrip.forgotten_cantrips.bubble_up.shallow_water"));
+            return;
+        }
+
         player.addEffect(new MobEffectInstance(
                 EffectRegistry.BUBBLE_UP.get(),
-                600,
+                240,
                 0,
                 false,
                 true,
                 true
         ));
+    }
+
+    private boolean hasEnoughWaterDepth(Player player) {
+        BlockPos pos = player.blockPosition();
+
+        return player.level().getBlockState(pos.above(2)).getBlock() == Blocks.WATER &&
+                player.level().getBlockState(pos.above(3)).getBlock() == Blocks.WATER;
+    }
+
+    private boolean isInBubbleColumn(Player player) {
+        BlockPos pos = player.blockPosition();
+        return player.level().getBlockState(pos).getBlock() == Blocks.BUBBLE_COLUMN;
     }
 }
