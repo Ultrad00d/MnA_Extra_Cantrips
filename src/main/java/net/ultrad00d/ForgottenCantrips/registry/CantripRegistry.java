47729ef3c53d049f9a0b3c71366c756553e21adf
Igor <ultradoodakk@gmail.com>
2026-07-12 00:51:59 +0300

    change: new cantrip registry structure

--- src/main/java/net/ultrad00d/ForgottenCantrips/registry/CantripRegistry.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/registry/CantripRegistry.java
@@ -5,7 +5,6 @@ import static net.minecraft.resources.ResourceLocation.fromNamespaceAndPath;
 import com.mna.api.cantrips.ICantrip;
 import com.mna.api.timing.DelayedEventQueue;
 import com.mna.api.timing.TimedDelayedEvent;
-import com.mna.api.tools.RLoc;
 
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementProgress;
@@ -23,73 +22,24 @@ import net.minecraft.world.entity.player.Player;
 
 public class CantripRegistry {
     private static final int ICON_SHOW_TIME = 50;
-
-    public static final int LIGHTNING_TIER = 3;
-    public static final int SPECTRAL_BED_TIER = 1;
-    public static final int SPECTRAL_BOAT_TIER = 1;
-    public static final int FORCE_CONSUME_TIER = 1;
-    public static final int SPECTRAL_DONKEY_TIER = 1;
+    private static final com.mna.cantrips.CantripRegistry REGISTRY = com.mna.cantrips.CantripRegistry.INSTANCE;
 
     public static void register() {
-        com.mna.cantrips.CantripRegistry registry = com.mna.cantrips.CantripRegistry.INSTANCE;
-
-        // Allowed shapes: [tier] "namespace:path"
-        // [1] mna:manaweave_patterns/square                       [2] mna:manaweave_patterns/knot
-        // [1] mna:manaweave_patterns/triangle                     [1] mna:manaweave_patterns/circle
-        // [3] mna:manaweave_patterns/split_triangle               [1] mna:manaweave_patterns/slash
-        // [3] mna:manaweave_patterns/bolt                         [2] mna:manaweave_patterns/knot2
-        // [4] mna:manaweave_patterns/hourglass                    [2] mna:manaweave_patterns/knot3
-        // [5] mna:manaweave_patterns/inverted_split_triangle      [4] mna:manaweave_patterns/star
-        // [1] mna:manaweave_patterns/backslash                    [2] mna:manaweave_patterns/diamond
-        // [5] mna:manaweave_patterns/infinity                     [2] mna:manaweave_patterns/knot4
-        // [3] mna:manaweave_patterns/inverted_triangle
-
-        // Lightning Cantrip
-        registry.registerCantrip(
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "lightning"),
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "textures/gui/cantrips/lightning.png"),
-                LIGHTNING_TIER,
-                (player, cantrip, hand) -> run(player, cantrip, hand, new LightningCantripLogic(), false),
-                ItemStack.EMPTY,
-                        RLoc.create("manaweave_patterns/square"), RLoc.create("manaweave_patterns/circle"), RLoc.create("manaweave_patterns/bolt")
-        ).setRequiredAdvancement(fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "uc1/part_3"));
-
-        // Spectral Bed Cantrip
-        registry.registerCantrip(
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "spectral_bed"),
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "textures/gui/cantrips/spectral_bed.png"),
-                SPECTRAL_BED_TIER,
-                (player, cantrip, hand) -> run(player, cantrip, hand, new SpectralBedCantripLogic(), false),
-                ItemStack.EMPTY,
-                RLoc.create("manaweave_patterns/knot2"), RLoc.create("manaweave_patterns/diamond")
-        ).setRequiredAdvancement(fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "uc2/part_3"));
-        // Spectral Boat Cantrip
-        registry.registerCantrip(
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "spectral_boat"),
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "textures/gui/cantrips/spectral_boat.png"),
-                SPECTRAL_BOAT_TIER,
-                (player, cantrip, hand) -> run(player, cantrip, hand, new SpectralBoatCantripLogic(), false),
-                ItemStack.EMPTY,
-                RLoc.create("manaweave_patterns/knot2"), RLoc.create("manaweave_patterns/diamond")
-        );
-        // Force Consume Cantrip
-        registry.registerCantrip(
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "force_consume"),
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "textures/gui/cantrips/force_consume.png"),
-                FORCE_CONSUME_TIER,
-                (player, cantrip, hand) -> run(player, cantrip, hand, new ForceConsumeCantripLogic(), true),
-                ItemStack.EMPTY,
-                RLoc.create("manaweave_patterns/knot2"), RLoc.create("manaweave_patterns/diamond")
-        );
-        // Spectral Donkey Cantrip
-        registry.registerCantrip(
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "spectral_donkey"),
-                fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "textures/gui/cantrips/spectral_donkey.png"),
-                SPECTRAL_DONKEY_TIER,
-                (player, cantrip, hand) -> run(player, cantrip, hand, new SpectralDonkeyCantripLogic(), false),
-                ItemStack.EMPTY,
-                RLoc.create("manaweave_patterns/knot2"), RLoc.create("manaweave_patterns/diamond")
-        ).setRequiredAdvancement(fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "uc3/part_3"));
+        for (var forgottenCantrip : CantripType.values()) {
+            ICantrip registered = REGISTRY.registerCantrip(
+                    fromNamespaceAndPath(ForgottenCantrips.MOD_ID, forgottenCantrip.getId()),
+                    fromNamespaceAndPath(ForgottenCantrips.MOD_ID, "textures/gui/cantrips/" + forgottenCantrip.getId() + ".png"),
+                    forgottenCantrip.getTier(),
+                    (player, cantrip, hand) -> run(player, cantrip, hand, forgottenCantrip.getLogic(), forgottenCantrip.isInstant()),
+                    ItemStack.EMPTY,
+                    forgottenCantrip.getShapes()
+            );
+            if (forgottenCantrip.getAdvancementPath() != null) {
+                registered.setRequiredAdvancement(
+                        fromNamespaceAndPath(ForgottenCantrips.MOD_ID, forgottenCantrip.getAdvancementPath())
+                );
+            }
+        }
     }
 
     public static boolean advancementCheck(Player player, ICantrip cantrip) {
