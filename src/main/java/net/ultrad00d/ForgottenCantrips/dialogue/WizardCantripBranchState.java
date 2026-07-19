94dee02227242f79b5efadc2dc8d773eb52862d6
dtamindarov5839 <d.tamindarov@innopolis.university>
2026-07-19 15:24:53 +0500

    Update WizardCantripBranchState.java

--- src/main/java/net/ultrad00d/ForgottenCantrips/dialogue/WizardCantripBranchState.java
+++ src/main/java/net/ultrad00d/ForgottenCantrips/dialogue/WizardCantripBranchState.java
@@ -2,11 +2,9 @@ package net.ultrad00d.ForgottenCantrips.dialogue;
 
 public enum WizardCantripBranchState {
     NOT_STARTED,
-    LOCATION_GIVEN,
-    ITEM_FOUND,
-    SPELL_LEARNED,
     PART_1,
     PART_2,
     PART_3,
-    PART_4;
+    PART_4,
+    SPELL_LEARNED;
 }
